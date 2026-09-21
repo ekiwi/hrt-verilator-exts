f7f7ef1f0a8a7cd1bc0f73ec60a71ab57952cb46
Kevin Läufer <laeufer@cornell.edu>
2026-09-21 11:01:06 -0400

    remove formal-utils

--- formal-utils/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Boolean finite-state machines, three-valued simulation, and AIGER I/O.
-
-pub mod formats;
-pub mod fsm;
-pub mod gate;
-pub mod option_bit_vec;
-pub mod sim;
-pub mod value;
-pub mod variable;
